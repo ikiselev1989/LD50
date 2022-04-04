f38d40786dca845acc538ce7b76094b8b6c56a49
Ilya Kiselev <ikiselev1989@gmail.com>
2022-04-04 13:46:03 +0300

    init

--- src/scenes/Loader.ts
+++ src/scenes/Loader.ts
@@ -10,23 +10,24 @@ export default class Loader extends Scene {
 	}
 
 	preload() {
-		this.load.image(Assets.Gonzologo, `/assets/${Assets.Gonzologo}.png`);
-		this.load.image(Assets.Phaserlogo, `/assets/${Assets.Phaserlogo}.png`);
-		this.load.image(Assets.MainScreen, `/assets/${Assets.MainScreen}.png`);
-		this.load.image(Assets.MainScreen2, `/assets/${Assets.MainScreen2}.png`);
+		this.load.image(Assets.Gonzologo, `assets/${Assets.Gonzologo}.png`);
+		this.load.image(Assets.Phaserlogo, `assets/${Assets.Phaserlogo}.png`);
+		this.load.image(Assets.MainScreen, `assets/${Assets.MainScreen}.png`);
+		this.load.image(Assets.MainScreen2, `assets/${Assets.MainScreen2}.png`);
+		this.load.image(Assets.MainScreen3, `assets/${Assets.MainScreen3}.png`);
 
-		this.load.audio(ObjectsTex.Pappers, `/assets/${ObjectsTex.Pappers}.mp3`);
-		this.load.audio(ObjectsTex.Shredder, `/assets/${ObjectsTex.Shredder}.mp3`);
-		this.load.audio(Assets.Over, `/assets/${Assets.Over}.mp3`);
+		this.load.audio(ObjectsTex.Pappers, `assets/${ObjectsTex.Pappers}.mp3`);
+		this.load.audio(ObjectsTex.Shredder, `assets/${ObjectsTex.Shredder}.mp3`);
+		this.load.audio(Assets.Over, `assets/${Assets.Over}.mp3`);
 
-		this.load.aseprite(CharacterTex.Cleaner, `/animations/${CharacterTex.Cleaner}.png`, `/animations/${CharacterTex.Cleaner}.json`);
-		this.load.aseprite(CharacterTex.Corrupt, `/animations/${CharacterTex.Corrupt}.png`, `/animations/${CharacterTex.Corrupt}.json`);
-		this.load.aseprite(ObjectsTex.Shredder, `/animations/${ObjectsTex.Shredder}.png`, `/animations/${ObjectsTex.Shredder}.json`);
-		this.load.aseprite(ObjectsTex.Cursor, `/animations/${ObjectsTex.Cursor}.png`, `/animations/${ObjectsTex.Cursor}.json`);
-		this.load.aseprite(ObjectsTex.Clock, `/animations/${ObjectsTex.Clock}.png`, `/animations/${ObjectsTex.Clock}.json`);
-		this.load.aseprite(ObjectsTex.CountDown, `/animations/${ObjectsTex.CountDown}.png`, `/animations/${ObjectsTex.CountDown}.json`);
+		this.load.aseprite(CharacterTex.Cleaner, `animations/${CharacterTex.Cleaner}.png`, `animations/${CharacterTex.Cleaner}.json`);
+		this.load.aseprite(CharacterTex.Corrupt, `animations/${CharacterTex.Corrupt}.png`, `animations/${CharacterTex.Corrupt}.json`);
+		this.load.aseprite(ObjectsTex.Shredder, `animations/${ObjectsTex.Shredder}.png`, `animations/${ObjectsTex.Shredder}.json`);
+		this.load.aseprite(ObjectsTex.Cursor, `animations/${ObjectsTex.Cursor}.png`, `animations/${ObjectsTex.Cursor}.json`);
+		this.load.aseprite(ObjectsTex.Clock, `animations/${ObjectsTex.Clock}.png`, `animations/${ObjectsTex.Clock}.json`);
+		this.load.aseprite(ObjectsTex.CountDown, `animations/${ObjectsTex.CountDown}.png`, `animations/${ObjectsTex.CountDown}.json`);
 
-		this.load.image(ObjectsTex.Pappers, `/assets/${ObjectsTex.Pappers}.png`);
+		this.load.image(ObjectsTex.Pappers, `assets/${ObjectsTex.Pappers}.png`);
 
 		this.load.image('tiles', `tiles.png`);
 
